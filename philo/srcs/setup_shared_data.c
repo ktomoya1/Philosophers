f100c779b720f95113d9afefebf2b845d6c82ccc
ktomoya <twbtomoya2@gmail.com>
2024-01-27 14:39:06 +0900

    add free_ret_null.c

--- philo/srcs/setup_shared_data.c
+++ philo/srcs/setup_shared_data.c
@@ -6,7 +6,7 @@
 /*   By: ktomoya <twbtomoya2@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 16:19:56 by ktomoya           #+#    #+#             */
-/*   Updated: 2024/01/16 20:04:05 by ktomoya          ###   ########.fr       */
+/*   Updated: 2024/01/26 15:28:52 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,14 @@ t_shared_data	*setup_shared_data(int num_of_philos)
 	if (shared == NULL)
 		return (NULL);
 	shared->num_of_philos = num_of_philos;
-	shared->is_dead = false;
+	shared->death_flag = false;
+	if (pthread_mutex_init(&shared->death_mutex, NULL) != SUCCESS)
+		return (free_ret_nul(shared));
 	i = 0;
 	while (i < num_of_philos)
 	{
 		if (pthread_mutex_init(&shared->forks[i], NULL) != SUCCESS)
-		{
-			free(shared);
-			return (NULL);
-		}
+			return (free_ret_nul(shared));
 		i++;
 	}
 	return (shared);
