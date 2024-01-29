b167761b7c7e71c9611f6bd10def836caf15236a
tom <134240277+ktomoya1@users.noreply.github.com>
2024-01-29 18:55:44 +0900

    move funcs to output.c

--- philo/srcs/output.c
+++ philo/srcs/output.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_message.c                                    :+:      :+:    :+:   */
+/*   output.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ktomoya <twbtomoya2@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/16 14:08:56 by ktomoya           #+#    #+#             */
-/*   Updated: 2024/01/29 17:28:27 by ktomoya          ###   ########.fr       */
+/*   Created: 2024/01/29 18:52:08 by ktomoya           #+#    #+#             */
+/*   Updated: 2024/01/29 18:53:41 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,11 @@ void	print_message(t_philo *philo, const char *msg)
 	if (is_alive(philo) == false)
 		return ;
 	printf("%ld %d %s\n", get_cur_time(), philo->id, msg);
+}
+
+int		put_err_ret(int error_code, const char *format)
+{
+	write(STDERR_FILENO, format, ft_strlen(format));
+	write(STDERR_FILENO, "\n", 1);
+	return (error_code);
 }
