320a85b98f48a4b66362728a4c58d38a53026fd6
tom <134240277+ktomoya1@users.noreply.github.com>
2024-01-29 18:49:55 +0900

    move funcs to condition.c

--- philo/srcs/condition.c
+++ philo/srcs/condition.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   is_hungry.c                                        :+:      :+:    :+:   */
+/*   condition.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ktomoya <twbtomoya2@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/17 19:27:52 by ktomoya           #+#    #+#             */
-/*   Updated: 2024/01/28 18:58:47 by ktomoya          ###   ########.fr       */
+/*   Created: 2024/01/29 18:44:43 by ktomoya           #+#    #+#             */
+/*   Updated: 2024/01/29 18:47:38 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,29 @@ bool	is_hungry(t_philo *philo)
 	if (elapsed_time > philo->time_to_die)
 		return (true);
 	return (false);
+}
+
+bool	has_eaten(t_philo *philo)
+{
+	if (philo->meal_count == philo->minimum_meal_count)
+		return (true);
+	return (false);
+}
+
+bool	is_alive(t_philo *philo)
+{
+	bool	is_alive;
+
+	pthread_mutex_lock(&philo->shared->death_mutex);
+	if (philo->shared->death_flag == false)
+		is_alive = true;
+	else
+		is_alive = false;
+	pthread_mutex_unlock(&philo->shared->death_mutex);
+	return (is_alive);
+}
+
+bool	is_alive_and_eating(t_philo *philo)
+{
+	return (is_alive(philo) == true && has_eaten(philo) == false);
 }
