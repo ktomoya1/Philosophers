b4667dca7611b009640319fd42749a76b58222a0
tom <134240277+ktomoya1@users.noreply.github.com>
2024-01-29 18:38:35 +0900

    move funcs to main.c

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ktomoya <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 15:45:21 by ktomoya           #+#    #+#             */
-/*   Updated: 2024/01/29 15:15:17 by ktomoya          ###   ########.fr       */
+/*   Updated: 2024/01/29 18:36:24 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,55 @@ int	main(int argc, char *argv[])
 		return (FAILURE);
 	wait_thread(threads);
 	free(shared);
+}
+
+t_shared_data	*setup_shared_data(int argc, int num_of_philos)
+{
+	t_shared_data	*shared;
+	int				i;
+
+	shared = (t_shared_data *)malloc(sizeof(t_shared_data));
+	if (shared == NULL)
+		return (NULL);
+	shared->num_of_philos = num_of_philos;
+	shared->death_flag = false;
+	if (pthread_mutex_init(&shared->death_mutex, NULL) != SUCCESS)
+		return (free_ret_nul(shared));
+	i = 0;
+	while (i < num_of_philos)
+	{
+		if (pthread_mutex_init(&shared->forks[i], NULL) != SUCCESS)
+			return (free_ret_nul(shared));
+		i++;
+	}
+	if (argc == 5)
+		shared->condition = is_alive;
+	else
+		shared->condition = is_alive_and_eating;
+	return (shared);
+}
+
+void	init_philo(t_philo philos[], char *argv[], t_shared_data *shared)
+{
+	int			i;
+
+	i = 0;
+	while (i < shared->num_of_philos)
+	{
+		philos[i].id = i + 1;
+		philos[i].fork_count = 0;
+		philos[i].right_fork = NULL;
+		philos[i].left_fork = NULL;
+		philos[i].meal_count = 0;
+		philos[i].start_time = 0;
+		philos[i].time_to_die = ft_atoi(argv[2]);
+		philos[i].time_to_eat = ft_atoi(argv[3]);
+		philos[i].time_to_sleep = ft_atoi(argv[4]);
+		if (argv[5] == NULL)
+			philos[i].minimum_meal_count = 0;
+		else
+			philos[i].minimum_meal_count = ft_atoi(argv[5]);
+		philos[i].shared = shared;
+		i++;
+	}
 }
