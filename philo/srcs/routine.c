a9669b3f39507868ba00a6028a9793c27c5ead33
ktomoya <twbtomoya2@gmail.com>
2024-02-13 14:58:03 +0900

    changed ft_usleep to usleep

--- philo/srcs/routine.c
+++ philo/srcs/routine.c
@@ -6,7 +6,7 @@
 /*   By: ktomoya <ktomoya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 17:50:38 by ktomoya           #+#    #+#             */
-/*   Updated: 2024/02/12 16:43:30 by ktomoya          ###   ########.fr       */
+/*   Updated: 2024/02/13 14:51:59 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*routine(void *arg)
 
 	philo = (t_philo *)arg;
 	while (get_cur_time() < philo->shared->program_start)
-		ft_usleep(1);
+		usleep(1);
 	if (philo->shared->num_of_philos == 1)
 	{
 		print_message(philo, "has taken a fork");
