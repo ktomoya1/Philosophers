53485c837643645e9ba266ab745aaadbf24a2ee4
ktomoya <twbtomoya2@gmail.com>
2024-02-16 14:53:46 +0900

    added structure

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: ktomoya <ktomoya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 15:46:22 by ktomoya           #+#    #+#             */
-/*   Updated: 2024/02/16 14:27:27 by ktomoya          ###   ########.fr       */
+/*   Updated: 2024/02/16 14:50:38 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,36 @@
 # define SUCCESS 0
 # define FAILURE 1
 # define ERROR -1
+
+typedef struct s_info	t_info;
+typedef struct s_philo	t_philo;
+
+struct s_info
+{
+	int			num_of_philos;
+	int			minimum_meal_count;
+	bool		someone_dead;
+	useconds_t	time_to_die;
+	useconds_t	time_to_eat;
+	useconds_t	time_to_sleep;
+	useconds_t	start_time;
+	pthread_mutex_t	someone_dead_mutex;
+	pthread_mutex_t	*is_full_mutex;
+	pthread_mutex_t	*meal_time_mutex;
+	pthread_mutex_t	*forks;
+}
+
+struct s_philo
+{
+	int			id;
+	int			lfork_id;
+	int			rfork_id;
+	int			meal_count;
+	bool		is_full;
+	bool		is_dead;
+	bool		someone_dead;
+	t_info		*info;
+	useconds_t	last_meal_time;
+}
 
 #endif
