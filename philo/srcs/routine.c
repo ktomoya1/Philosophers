752c773d1c386d3172a9ed75d674463780172172
ktomoya <twbtomoya2@gmail.com>
2024-02-22 16:39:05 +0900

    handled norm

--- philo/srcs/routine.c
+++ philo/srcs/routine.c
@@ -6,7 +6,7 @@
 /*   By: ktomoya <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 10:55:43 by ktomoya           #+#    #+#             */
-/*   Updated: 2024/02/22 15:27:25 by ktomoya          ###   ########.fr       */
+/*   Updated: 2024/02/22 16:33:12 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,10 @@ void	*routine(void *args)
 	t_philo	*philo;
 
 	philo = (t_philo *)args;
-	pthread_mutex_lock(&philo->info->time_mutex[philo->id - 1]);
-	philo->last_meal_time = philo->info->start_time;
-	pthread_mutex_unlock(&philo->info->time_mutex[philo->id - 1]);
 	while (get_time() < philo->info->start_time)
 		usleep(1);
 	if (philo->info->someone_dead == true)
-	   return ((void *)1);
+		return ((void *)1);
 	if (philo->info->num_of_philos == 1)
 	{
 		print_message("has_taken a fork", philo);
--- philo/srcs/thread.c
+++ philo/srcs/thread.c
@@ -6,7 +6,7 @@
 /*   By: ktomoya <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 09:48:20 by ktomoya           #+#    #+#             */
-/*   Updated: 2024/02/22 15:44:22 by ktomoya          ###   ########.fr       */
+/*   Updated: 2024/02/22 16:34:30 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,9 @@ int	create_threads(t_info *info, t_philo *philos, int philo_count)
 	info->start_time = get_time() + 1000;
 	while (i < philo_count)
 	{
+		pthread_mutex_lock(&info->time_mutex[philos[i].id - 1]);
+		philos[i].last_meal_time = info->start_time;
+		pthread_mutex_unlock(&info->time_mutex[philos[i].id - 1]);
 		status = pthread_create(&info->threads[i], NULL, routine, &philos[i]);
 		if (status != SUCCESS)
 		{
